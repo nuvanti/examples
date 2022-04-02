4860c1ed1d6f351393aeabdbaf076a43a519c82b
Thomas Jenkins <rhaug@quantumsolutionsllc.com>
2022-04-02 15:18:07 -0400

    initial test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # examples
-Nuvanti example applications
+This repository contains official Nuvanti example applications.
+
+- northwind: The customer/products/vendor sample database application implemented in Nuvanti
+- simple_crm: A simple CRM (customer relationship management) that leverages Nuvanti's higher level CRM model
+- starter: Empty RAD model based application
---
+++ northwind/app.js
@@ -0,0 +1,60 @@
+import { createBasicUiModel } from "@nuvanti/basic-ui-model";
+import { createRadModel } from "@nuvanti/rad-model";
+import {} from "@nuvanti/ui";
+import { modify } from "@nuvanti/ts-style";
+
+const { radModel, addTable, generateFromRad } = createRadModel({
+  name: "Nuvanti",
+  id: "AMacekin",
+  defaultFieldDisplay: () => {
+    throw "";
+  },
+  defaultFormField: () => {
+    throw "";
+  },
+});
+
+addTable("User", (table) => {
+  table.nuvaId("globalId").nonNull().unique();
+  table.bool("disabled").nonNull();
+});
+
+addTable("Category", (table) => {
+  table.string("name", 15);
+  table.string("description", 500);
+  table.selectFormControl();
+});
+
+const { addPage, generateUi } = createBasicUiModel();
+
+// addPage({
+//   path: "/",
+//   content: simpleEditableGridPage({ table: "Category", radModel }),
+// });
+
+// const shell = simpleNavbar({
+//   links: [
+//     { label: "Home", path: "/" },
+//     { label: "Categories", path: "/categories" },
+//     { label: "Contacts", path: "/contacts" },
+//     { label: "Peeps", path: "/peeps" },
+//   ],
+// });
+
+/** @type {import("@nuvanti/runtime-model").RuntimeModel} */
+export const app = {
+  ...generateFromRad(),
+  ui: generateUi(shell, createUiModel),
+  runProfile: [
+    {
+      asUser: `AMacekin`,
+      name: "default",
+      procedure: [
+        modify(
+          `insert into db.User (globalId, disabled) values (cast('AMacekin' as NuvaId), false)`
+        ),
+      ],
+      time: `2021-11-11T16:37:49.715Z`,
+    },
+  ],
+};
